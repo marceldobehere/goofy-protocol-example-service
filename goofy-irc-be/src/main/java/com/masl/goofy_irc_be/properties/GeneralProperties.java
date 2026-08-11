bd0d6a438092f00b7d71f23bf410cdd422a2feb7
marceldobehere <marcel.z2006@gmail.com>
2026-08-11 23:16:40 +0200

    ver

--- goofy-irc-be/src/main/java/com/masl/goofy_irc_be/properties/GeneralProperties.java
+++ goofy-irc-be/src/main/java/com/masl/goofy_irc_be/properties/GeneralProperties.java
@@ -14,7 +14,7 @@ public class GeneralProperties {
     private String name;
     private String description;
     private String contact;
-    public final static String version = "0.5.2";
+    public final static String version = "0.7.1";
 
     public String getDomain() {
         URI uri = URI.create(url);
