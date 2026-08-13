9b9c2d21e056bb0cfc645fee721df427d5451033
marceldobehere <marcel.z2006@gmail.com>
2026-08-13 02:11:04 +0200

    should hopefully avoid future http2 issues (I pray)

--- goofy-irc-be/src/main/java/com/masl/goofy_irc_be/rest/RootEndpoint.java
+++ goofy-irc-be/src/main/java/com/masl/goofy_irc_be/rest/RootEndpoint.java
@@ -3,6 +3,7 @@ package com.masl.goofy_irc_be.rest;
 import com.masl.goofy_irc_be.exception.base.swagger.IrcEndpoint;
 import com.masl.goofy_irc_be.properties.GeneralProperties;
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +23,11 @@ public class RootEndpoint {
 
     @GetMapping
     @IrcEndpoint(summary = "Redirects to the Frontend URL (should be static) <br>Also appends the `overrideBackendUrl` automatically, so the Frontend talks to the correct Backend")
-    public ResponseEntity<String> index() {
+    public ResponseEntity<String> index(HttpServletRequest request) {
+        // Important due to HTTP/2 potentially reusing the connection even though it is on a different domain, therefore causing an infinite redirection loop!
+        if (!generalProperties.getDomainHost().equals(request.getServerName()))
+            return ResponseEntity.status(HttpStatus.MISDIRECTED_REQUEST).build();
+
         URI redirectUri = UriComponentsBuilder
                 .fromUriString(generalProperties.getFrontendUrl())
                 .queryParam("overrideBackendUrl", generalProperties.getUrl())
